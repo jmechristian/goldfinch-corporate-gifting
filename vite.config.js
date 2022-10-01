4083e176ceebe78f2b000d9a989a46f45cebde0e
jmechristian <jmechristian@gmail.com>
2022-10-01 18:09:09 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsDir: '/assets',
-    sourcemap: false,
-    define: {
-      'process.env': {},
-    },
-    lib: {
-      entry: 'src/main.jsx',
-      formats: ['iife'],
-      name: 'gfCorpGifting',
-      fileName: 'gf-corp-gifting.js',
-    },
-  },
 });
